340aa71a0bc17c829765e44df65f3e7dd317e440
Mohindar <mohindarv@gmail.com>
2020-03-31 19:34:10 +0530

    login page table create

--- login.php
+++ login.php
@@ -7,6 +7,18 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	
+	<table>
+		<caption>table title and/or explanatory text</caption>
+		<thead>
+			<tr>
+				<th>header</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>data</td>
+			</tr>
+		</tbody>
+	</table>
 </body>
 </html>
